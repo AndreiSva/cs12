2e1b886bd459abda0c617eefdaee0d4ec0da9fae
Andrei <andreisva2023@gmail.com>
2023-02-18 01:34:00 -0800

    add minimax function

--- reversi_ai/reversi.py
+++ reversi_ai/reversi.py
@@ -1,9 +1,10 @@
+
 # Reversi AI
 
 import random
 import time
 import copy
-import itertools
+from itertools import repeat
 
 board = [[" ", " ", " ", " ", " ", " ", " ", " "],
          [" ", " ", " ", " ", " ", " ", " ", " "],
@@ -96,20 +97,20 @@ def evaluate(board):
         w += row.count("○")
     return b - w
 
-def max_moves(board, player):
-    """Returns a list of tuples (x, y) representing the moves that have
-       the highest evaluation score for the given player."""
+def get_enemy(player):
+    return white if player == black else black
+
+# this function is useful because it reduces code duplication between the max_moves and minmax_moves
+def get_moves(board, player):
+    """Returns a list of tuples that has all possible moves for a player,
+       as well as a list of their evaluations"""
     possiblemoves = []
     possiblescores = []
     tried= []
 
     # this is needed for the useless optimization
-    enemy = None
-    if player == black:
-        enemy = white
-    else:
-        enemy = black
-
+    enemy = get_enemy(player)
+    
     # we need to get every possible move for our turn
     # this loop is optimized to only search through fresh tiles adjacent to enemy tiles
     # the optimization doesn't matter that much since the board is only 7x7
@@ -129,32 +130,82 @@ def max_moves(board, player):
                                 possiblemoves.append(((j+x, i+y)))
                                 possiblescores.append(evaluate(bboard))
                             tried.append((j+x, i+y))
-    # this used to be a "clever" single line return statement
-    ret = []
-    for i in range(len(possiblescores)):
-        if ((player == black and possiblescores[i] == max(possiblescores)) or
-            (player == white and possiblescores[i] == min(possiblescores))):
-            ret.append(possiblemoves[i])
-    return ret
+                            
+    return (possiblemoves, possiblescores)
+    
+def max_moves(board, player):
+    """Returns a list of tuples (x, y) representing the moves that have
+       the highest evaluation score for the given player."""
+    moves, scores = get_moves(board, player)
+
+    if len(moves) == 0:
+        return []
+
+    # what we're looking for
+    target = max(scores) if player == black else min(scores)
+
+    # we need only the coordinates that have our target value
+    return list(filter(lambda x : scores[moves.index(x)] == target, moves))
 
 def minimax_moves(board, player):
     """Returns a list of tuples (x, y) representing the moves that give
        the opposing player the worst possible evaluation score for the
        best of their next moves."""
 
+    enemy = get_enemy(player)
+
+    # this is the function we will use to find what we are looking for
+    # (worst score for them)
+    target_func = min if enemy == black else max
+    
+    # the moves that we can make
+    moves = get_moves(board, player)[0]
+
+    # the scores that the enemy will have after our move
+    scores = []
+    for move in moves:
+        bboard = copy.deepcopy(board)
+
+        # play our move
+        play(bboard, player, move[0], move[1])
+
+        # the enemy's best move
+        enemy_moves = max_moves(bboard, enemy)
+
+        # the enemy is not able to move after this turn
+        # (win for us)
+        if len(enemy_moves) == 0:
+            return [move]
+
+        enemy_move = enemy_moves[0]
+            
+        # play the enemy's best move
+        play(bboard, get_enemy(player), enemy_move[0], enemy_move[1])
+        scores.append(evaluate(bboard))
+
+    return list(filter(lambda x : scores.index(target_func(scores)) == moves.index(x), moves))
+
 # -----------------------
 # -----------------------
 def main():
     current_player = "●"
     while True:
+        display(board)
         if current_player == "●":
             moves = max_moves(board, current_player)
         else:
             moves = minimax_moves(board, current_player)
         if moves == []:
+            display(board)
+            score = evaluate(board)
+            if score > 0:
+                print("black wins")
+            else:
+                print("white wins")
             break
         else:
             # Play a random move from the best moves
+            print(moves, current_player)
             x, y = random.choice(moves)
             play(board, current_player, x, y)
             current_player = next_player(current_player)
